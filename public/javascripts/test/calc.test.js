7df2b0f52e52fcde3f1d89c5bc999c7d7166abf5
simo215d <simo215d@edu.easj.dk>
2020-10-23 17:29:21 +0200

    updated test class to include error to check pipeline reaction

--- public/javascripts/test/calc.test.js
+++ public/javascripts/test/calc.test.js
@@ -1,6 +1,6 @@
 const sum = require('../calc');
  
 test('adds 1 + 2 to equal 3', () => {
- expect(sum(2, 2)).toBe(4);
+ expect(sum(2, 2)).toBe(5);
 });
 //mere tutorial: https://jestjs.io/docs/en/getting-started
